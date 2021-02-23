a953f3a108503ab886ffd524e65eb80ed0d1b15d
Isak Fyksen <isak@obsidian.no>
2021-02-23 03:33:17 +0100

    Print using display segment

--- bootblock.asm
+++ bootblock.asm
@@ -1,29 +1,69 @@
-%define BOOT_SEGMENT 0x07c0
-%define ENDL         10, 13
+[ORG 0x7c00]
 
-    mov ax, BOOT_SEGMENT
+%define DISPLAY_SEGMENT 0xb800
+%define STACK_POINTER   0x9c00
+
+%define TERM_FG 10
+%define TERM_BG 0
+%define ENDL    10, 13
+
+_start:
+    xor ax, ax
     mov ds, ax
+    mov ss, ax
+    mov sp, STACK_POINTER
+
     cld
 
+    mov ax, DISPLAY_SEGMENT
+    mov es, ax
+
     mov si, msg
-    call BIOS_print
+    call println
 
 hang:
     jmp hang
 
 msg:
-    db "Hello world!", ENDL, 0
+    db "Hello world!", 0
 
-BIOS_print:
-    lodsb
-    or al, al
-    jz BIOS_print_done
-    mov ah, 0x0e
-    mov bh, 0
-    int 0x10
-    jmp BIOS_print
-BIOS_print_done:
+; === Functions ===
+
+println_next: call putc ; Write next char
+println:
+    lodsb               ; Load next char
+    cmp al, 0           ; Check if null
+    jne println_next    ; Print if not, else:
+    mov byte [xpos], 0  ; Carriage return
+    add byte [ypos], 1  ; Line feed
+    ret                 ; Return
+
+putc:
+    mov ah, TERM_BG ; Set background color
+    shl ah, 4       ; ah <<= 4
+    or  ah, TERM_FG ; Set foreground color
+    mov cx, ax      ; Store char/color
+    
+    movzx ax, byte [ypos]   ; ax = ypos
+    mov   dx, 160           ; dx = 160 (2 bytes times 80 columns)
+    mul   dx                ; ax *= dx
+    movzx bx, byte [xpos]   ; bx = xpos
+    shl   bx, 1             ; bx *= 2 (2 bytes for each character)
+
+    xor di, di  ; di = 0
+    add di, ax  ; di += ax
+    add di, bx  ; di += bx
+
+    mov ax, cx          ; Load char/color
+    stosw               ; Write char/color
+    add byte [xpos], 1  ; Move the cursor
+
     ret
+
+; === Variables ===
+
+xpos:   db 0
+ypos:   db 0
 
 sign:
     times 510-($-$$) db 0
