c98c44ee2d86265ce93bfd6289f8224834790613
Isak Fyksen <isak@obsidian.no>
2021-02-23 02:37:49 +0100

    Print with function

--- bootblock.asm
+++ bootblock.asm
@@ -1,26 +1,29 @@
 %define BOOT_SEGMENT 0x07c0
 %define ENDL         10, 13
 
-
     mov ax, BOOT_SEGMENT
     mov ds, ax
-    mov si, msg
     cld
 
-ch_loop:
-    lodsb
-    or al, al
-    jz hang
-    mov ah, 0x0e
-    mov bh, 0
-    int 0x10
-    jmp ch_loop
+    mov si, msg
+    call BIOS_print
 
 hang:
     jmp hang
 
 msg:
     db "Hello world!", ENDL, 0
+
+BIOS_print:
+    lodsb
+    or al, al
+    jz BIOS_print_done
+    mov ah, 0x0e
+    mov bh, 0
+    int 0x10
+    jmp BIOS_print
+BIOS_print_done:
+    ret
 
 sign:
     times 510-($-$$) db 0
